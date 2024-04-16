d308aaba03a5fb888946a47cc5bbcd9f1d04256d
robertBuechner <93946086+robertBuechner@users.noreply.github.com>
2024-04-16 15:00:31 -0500

    Create install_queries.sh

---
+++ financial_crime/application_fraud/queries/install_queries.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the graph name
+GRAPH_NAME="Application_Fraud"
+
+# Directory containing your .gsql files
+QUERY_DIR="./queries"
+
+# Iterate over each .gsql file in the directory
+for file in "$QUERY_DIR"/*.gsql; do
+    echo "Running $file..."
+    gsql -g "$GRAPH_NAME" "$file"
+done
+
+echo "All scripts have been executed."
+
+gsql --graph "$GRAPH_NAME" INSTALL QUERY ALL
